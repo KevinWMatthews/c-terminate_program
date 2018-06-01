0674e80474d5c3a33ddc31af69aa0dd256803f8d
Kevin W Matthews <KevinWMatthews@gmail.com>
2018-06-01 09:34:56 -0700

    Add exit with cleanup functions

---
+++ terminate_with_cleanup.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+void cleanup_1(void)
+{
+    printf("In %s\n", __func__);
+}
+
+void cleanup_2(void)
+{
+    printf("In %s\n", __func__);
+}
+
+int main(void)
+{
+    // Cleanup functions are executed in reverse registration order, on success only
+    atexit(cleanup_1);
+    atexit(cleanup_2);
+
+    // exit(EXIT_SUCCESS);
+    exit(EXIT_FAILURE);
+    return 0;
+}
